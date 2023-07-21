3dfcc8141e8b73ccf4d8d62bd24a06631c690023
Seabelom1 <seabelomorena@gmai.com>
2023-07-21 13:31:46 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,12 @@
 /**
  * _isupper-checks for uppercase charaters
  * @c: function parameter
- * Return: 1 on success and 0 on faliure
+ * Return: 1 on success and 0 on failure
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
-	return (1);
-	}
-	return (0);
+		return (1);
+	else
+		return (0);
 }
